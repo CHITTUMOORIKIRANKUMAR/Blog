f0c0a361377047a8f629c77bb50f1c029c0a05cd
kiran <chittimurikirankumar@gmail.com>
2024-08-29 22:28:12 +0530

    Created SignIn API route

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
 import {errorHandler} from '../utils/error.js'
+import jwt from 'jsonwebtoken';
 
 
 export const signup = async (req, res,next)=>{
@@ -28,3 +29,30 @@ export const signup = async (req, res,next)=>{
     
 
 }
+
+export const signin = async (req,res, next)=>{
+    const {email, password} =req.body;
+    if(!email ||!password || email===''||password===''){
+        next(errorHandler(400,"All Fields are required"));
+    }
+        try{
+            const validUser = await User.findOne({email});
+            if(!validUser){
+                return next(errorHandler(404,'User not found'));
+            }
+            const validPassword = bcryptjs.compareSync(password,validUser.password);
+            if(!validPassword){
+                return next(errorHandler(400,'Invalid password'))
+            }
+            const token =jwt.sign({id:validUser._id},process.env.JWT_SECRET);
+            const {password:pass, ...res} =validUser._doc;
+             res.status(200).cookie('access_token',token,{
+                httpOnly:true,
+            }).json(validUser);
+
+
+        }catch(error){
+            next(error)
+        }
+    }
+
