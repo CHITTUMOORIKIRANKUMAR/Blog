96ee3b9db45adb9e44394e6ffc287d956c72235e
kiran <chittimurikirankumar@gmail.com>
2024-08-29 12:25:08 +0530

    created footer component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,68 @@
+import { Footer } from 'flowbite-react'
+import React from 'react'
+import { Link } from 'react-router-dom'
+ import { FaGithub } from "react-icons/fa";
+
+const FooterCom = () => {
+  return (
+    <Footer container className='border border-t-8 border-teal-500'>
+      <div className='w-full max-w-7xl mx-auto'>
+        <div className='grid w-full justify-between sm:flex md:grid-cols-1'>
+            <div className='mt-5 '>
+            <Link to ="/" className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'>
+        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+            Kiran's
+        </span>
+        Blog
+        </Link>
+            </div>
+            <div className='grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6'>
+                <div>
+                <Footer.Title title='About' />
+                <Footer.LinkGroup col>
+                    <Footer.Link href='/projects' target='_blank' rel='noopener noreferrer'>
+                        Projects
+                    </Footer.Link>
+                    <Footer.Link href='/about' target='_blank' rel='noopener noreferrer'>About</Footer.Link>
+                </Footer.LinkGroup>
+                </div>
+                <div>
+                <Footer.Title title='Legal' />
+                <Footer.LinkGroup col>
+                    <Footer.Link href='#' target='_blank' rel='noopener noreferrer'>
+                        GitHub
+                    </Footer.Link>
+                    <Footer.Link href='#' target='_blank' rel='noopener noreferrer'>
+                        Linked In
+                        </Footer.Link>
+                </Footer.LinkGroup>
+                </div>
+                <div>
+                <Footer.Title title='Legal' />
+                <Footer.LinkGroup col>
+                    <Footer.Link href='#' target='_blank' rel='noopener noreferrer'>
+                        Privacy policy
+                    </Footer.Link>
+                    <Footer.Link href='#' target='_blank' rel='noopener noreferrer'>Terms & Conditions</Footer.Link>
+                </Footer.LinkGroup>
+                </div>
+            </div>
+        </div>
+        <Footer.Divider />
+        <div className=" w-full sm:flex sm:items-center sm:justify-between">
+            <Footer.Copyright href='#' by="Kiran's Blog" year={new Date().getFullYear()}/>
+            <div className='flex gap-6 sm:mt-0 mt-4 sm:justify-center'>
+            
+                <Footer.Icon href='#' icon={FaGithub} /> 
+                <Footer.Icon href='#' icon={FaGithub} /> 
+                <Footer.Icon href='#' icon={FaGithub} /> 
+                <Footer.Icon href='#' icon={FaGithub} /> 
+            
+            </div>
+        </div>
+      </div>
+    </Footer>
+  )
+}
+
+export default FooterCom
