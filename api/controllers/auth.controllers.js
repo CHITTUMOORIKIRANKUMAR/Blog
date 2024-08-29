a85a9c5de96b21f9dd82eb8ea858938718af6e1b
kiran <chittimurikirankumar@gmail.com>
2024-08-29 22:48:32 +0530

    Sign In Page Functionality

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -45,7 +45,7 @@ export const signin = async (req,res, next)=>{
                 return next(errorHandler(400,'Invalid password'))
             }
             const token =jwt.sign({id:validUser._id},process.env.JWT_SECRET);
-            const {password:pass, ...res} =validUser._doc;
+            //const {password:pass, ...res} =validUser._doc;
              res.status(200).cookie('access_token',token,{
                 httpOnly:true,
             }).json(validUser);
